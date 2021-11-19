544d14ddd3f66c8cfcd9f82f06662aebb59a4952
Coke <a.idzikowski@student.uw.edu.pl>
2021-11-19 21:41:15 +0100

    Continue working on Projects page, add individual project save

---
+++ CokesPortfolio/Shared/Models/ProjectsModel.cs
@@ -0,0 +1,16 @@
+﻿
+namespace CokesPortfolio.Shared.Models
+{
+    public class ProjectsModel
+    {
+        public bool IsArturExpanded { get; set; }
+        public bool IsGtaDiscordExpanded { get; set; }
+        public bool IsCokeBanExpanded { get; set; }
+        public bool IsBuffysDiscordExpanded { get; set; }
+        public bool IsDiscordApiExpanded { get; set; }
+        public bool IsAppUploaderExpanded { get; set; }
+        public bool IsGtaMissionExpanded { get; set; }
+        public bool IsEmailExpanded { get; set; }
+        public bool IsBmNotifierExpanded { get; set; }
+    }
+}
