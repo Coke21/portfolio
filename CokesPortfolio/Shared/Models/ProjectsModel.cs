27543d7e8fb88a7b674c20dd2ac6ece450b513ec
Artur <a.idzikowski@student.uw.edu.pl>
2021-12-16 14:59:22 +0100

    Finish VariablePatcher, add InlineSkatesManager project

--- CokesPortfolio/Shared/Models/ProjectsModel.cs
+++ CokesPortfolio/Shared/Models/ProjectsModel.cs
@@ -4,13 +4,18 @@ namespace CokesPortfolio.Shared.Models
     public class ProjectsModel
     {
         public bool IsArturExpanded { get; set; }
+        public bool IsVariablePatcherExpanded { get; set; }
+
         public bool IsGtaDiscordExpanded { get; set; }
         public bool IsCokeBanExpanded { get; set; }
         public bool IsBuffysDiscordExpanded { get; set; }
         public bool IsDiscordApiExpanded { get; set; }
+
         public bool IsAppUploaderExpanded { get; set; }
         public bool IsGtaMissionExpanded { get; set; }
         public bool IsEmailExpanded { get; set; }
+
+        public bool IsInlineSkatesManagerExpanded { get; set; }
         public bool IsBmNotifierExpanded { get; set; }
 
         public bool IsGameCpExpanded { get; set; }
